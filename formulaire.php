8e7046f8cdd9afa5ef399c74389884af22a89357
saracalafuri <saracalafuri@gmail.com>
2017-12-07 23:17:28 +0100

    début du formulaire

---
+++ formulaire.php
@@ -0,0 +1,19 @@
+﻿<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<title>Est-ce que tu peux conduire ?</title>
+	</head>
+	
+	<body>
+		<h1>Est-ce que tu peux conduire ?</h1>
+		
+		<form>
+			 <select name="Genre">
+				<option value="masculin">Masculin</option>
+				<option value="feminin">Féminin</option>
+				<option value="autre">Autre</option>
+			</select> 
+		</form>
+	</body>
+</html>
