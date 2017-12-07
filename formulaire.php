3444419aba613b51b88586e008443ffc4eaf0274
saracalafuri <saracalafuri@gmail.com>
2017-12-08 00:36:59 +0100

    html plus ou moins terminé, manque le php

--- formulaire.php
+++ formulaire.php
@@ -9,32 +9,42 @@
 		<h1>Est-ce que tu peux conduire ?</h1>
 		
 		<!-- Formulaire -->
-		<form>
+		<form method="post" action="formulaire.php">
+			Tu es :
 			<!-- Menu déroulant : genre -->
-			Ton genre :
-			 <select name="Genre">
-				<option value="masculin">Masculin</option>
-				<option value="feminin">Féminin</option>
-				<option value="autre">Autre</option>
+			<select name="genre">
+				<option value="masculin">un homme</option>
+				<option value="feminin">une femme</option>
+				<option value="autre">autre</option>
 			</select> 
-			<br /> <br />
+			<br /><br />
 			
-			<!-- Slider : de 0 à X verres //-->
-			 Tu as bu :
-			<input type="range" value="0" list="graduations" min="0" max="20" />
+			 Tu as bu : 0
+			<!-- Slider : de 0 à 10+ verres -->
+			<input name="nbverres" type="range" value="0" list="graduations" min="0" max="10" />
+			<!-- graduations non supportées par Firefox -->
 			<datalist id="graduations">
-				<option value="0" label="0 verre" />
+				<option value="0" />
 				<option value="1" />
 				<option value="2" />
 				<option value="3" />
 				<option value="4" />
-				<option value="5" label="5 verres" />
+				<option value="5" />
 				<option value="6" />
 				<option value="7" />
 				<option value="8" />
 				<option value="9" />
-				<option value="10" label="10 verres"/>
-			</datalist>
+				<option value="10" />
+			</datalist> 10+ verres
+			<br /><br />
+			
+			Tu pèses :
+			<!-- Champ de texte : poids en kg -->
+			<input name="poids" type="text" value="ton poids en kg" min="0" max="200" />
+			<br /><br />
+			
+			<!-- Bouton d'envoi du formulaire -->
+			<input type="submit" value ="Alors ?" />
 		</form>
 	</body>
 </html>
