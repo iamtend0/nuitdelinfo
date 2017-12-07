b8fa917999a4b01c58b3447831b1e62734964c3b
nicolai <nicolai@dagestad.fr>
2017-12-07 23:23:55 +0100

    Début de la branche radio

---
+++ header.php
@@ -0,0 +1,15 @@
+<header>
+	<nav>
+		<ul>
+			<li><a>Home</a></li>
+			<li><a>Sam</a></li>
+			<li><a>Une page introuvable?</a></li>
+			<li><a>Jeu limites de vitesses</a></li>
+			<li><a>Est-ce que tu peut conduire</a></li>
+		</ul>
+	</nav>
+
+	<div id="radio">
+		
+	</div>
+</header>
