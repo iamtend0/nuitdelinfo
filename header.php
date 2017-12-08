625b51e69c19022345643e302d930e0158bd9d38
nicolai <nicolai@dagestad.fr>
2017-12-08 06:22:30 +0100

    ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ

--- header.php
+++ header.php
@@ -1,27 +1,26 @@
-<header id="header">
-	<div class="logo"><a href="index.php">APPG</a></   div>
-	<a href="#menu">Menu</a>
+<header id="main">
+	<nav>
+		<a href="index.php">APPG</a>
+		<a href="sam.php" >Sam</a>
+		<a href="vnios" >Une page introuvable?</a>
+		<a href="formulaire.php" >Est-ce que tu peux conduire</a>
+		<div class="animation start-home"></div>
+	</nav>
 </header>
 <div id="radio">
-	<audio controls>
-		<source id="radioSource" type="audio/mpeg" src="http://listen.radioking.com/radio/116593/stream/155905">
-		Your browser does not support this audio format
-	</audio>
-	<input type="button" onclick="getInfo()" />
-	<div id="music-info" >
-		<ul>
-			<img src="" alt="Cover" id="cover"/>
-			<li id="title"></li>
-			<li id="artist"></li>
-		</ul>
-	</div>
+	<span id="bar">
+		<audio>
+			<source id="radioSource" type="audio/mpeg" src="http://listen.radioking.com/radio/116593/stream/155905">
+			Your browser does not support this audio format
+		</audio>
+		<input type="button" onclick="getInfo()" />
+		<div id="musicInfo" >
+			<ul>
+				<img src="" alt="Cover" id="cover"/>
+				<li id="title"></li>
+				<li id="artist"></li>
+			</ul>
+		</div>
+	</span>
 </div>
-<nav id="menu">
-	<ul class="links">
-		<li><a>Sam</a></li>
-		<li><a>Une page introuvable?</a></li>
-		<li><a>Jeu limites de vitesses</a></li>
-		<li><a>Est-ce que tu peux conduire</a></li>
-	</ul>
-</nav>
 <script src="js/radio.js" type="text/javascript"></script>
--- js/radio.js
+++ js/radio.js
@@ -3,10 +3,11 @@ function getInfo(){
 	request.open("GET", "https://www.radioking.com/widgets/currenttrack.php?radio=116593&format=json", false)
 	request.send();
 
-	document.getElementById("music-info").style.display="block";
+	document.getElementById("musicInfo").style.display="block";
 	var info = JSON.parse(request.responseText);
 
 	document.getElementById("title").innerHTML=info.title;
 	document.getElementById("artist").innerHTML=artist;
-	document.getElementById("cover").src=info.cover;
+	var cover=document.getElementById("cover")
+	cover.src=info.cover;
 }
