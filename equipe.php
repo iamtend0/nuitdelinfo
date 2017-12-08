6f2c953ff100e1ab7c8c5c3902c892a853b6026b
borde <martin.borde19@gmail.com>
2017-12-08 04:48:41 +0100

    Je suis un canard

---
+++ equipe.php
@@ -0,0 +1,53 @@
+<?php
+
+session_start(); // On démarre la session AVANT toute chose
+
+?>
+ <!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Qui sera Sam ?</title>
+		<link rel="stylesheet" href="assets/css/main.css" />
+	</head>
+<body class="subpage">
+	<?php include("header.php");?>
+	
+	<!-- One -->
+	<section id="One" class="wrapper style3">
+				<div class="inner">
+					<header class="align-center">
+						<p>APPG</p>
+						<h2>Équipe</h2>
+					</header>
+				</div>
+			</section>
+
+				<!-- Two -->
+			<section id="two" class="wrapper style2">
+				<div class="inner">
+					<div class="box">
+						<div class="content">
+							<header class="align-center">
+								<p>Sécurité routière</p>
+								<h2>Membres</h2>
+							</header>
+							<div class="align-center">
+							</br>
+							<p>BORDE Martin</p></br>
+							<p>CALAFURI Sara</p></br>
+							<p>DAGESTAD Nicolai</p></br>
+							<p>PUGET Aurélien</p></br>
+							<p>ROGÉ Clément</p></br>
+							</div>
+							</form>
+						</div>
+					</div>
+				</div>
+			</section>
+
+		
+		
+	<footer></footer>
+</body>
+</html> 
