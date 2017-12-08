e4a46a9d89f041e78d2a155133279c7711457413
borde <martin.borde19@gmail.com>
2017-12-08 01:30:23 +0100

    Vesrion de 01/30/34

--- sam.php
+++ sam.php
@@ -1,14 +1,23 @@
+ <?php
+session_start();
+$_SESSION['nb'];
+?>
+
+ 
  <!DOCTYPE html>
 <html>
 	<head>
-		<title>Qui sera Sam</title>
+		<meta charset="utf-8">
+		<title>Qui sera Sam ?</title>
 	</head>
 <body>
-
-
-	<?php
-	echo "My first PHP script!";
-	?>
-
+	<header></header>
+		<h1>Qui sera le prochain ?</h1>
+		<form method="post" action="noms.php">
+			<p>Combien etes vous a sortir ?</p>
+			<input type="number" name="howmuch">
+			<input type="submit" value="Valider" />
+		</form>
+	<footer></footer>
 </body>
 </html> 
