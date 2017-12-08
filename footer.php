d7b19f55038ae9e40bdc351f8845f2a0de5109b0
borde <martin.borde19@gmail.com>
2017-12-08 06:24:49 +0100

    moi

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
 						<li><a href="equipe.php" class="copyright"><span class="label">Equipe</span></a></li>
 						<li><a href="http://www.securite-routiere.gouv.fr/" class="copyright"><span class="label">Sécurité routière</span></a></li>
 						<li><a href="urgence.php" class="copyright"><span class="label">Numero Urgence</span></a></li>
-						<li><a href="https://www.nuitdelinfo.com/" class="copyright"><span class="label">Nuit des étoiles</span></a></li>
+						<li><a href="https://www.nuitdelinfo.com/" class="copyright"><span class="label">Nuit de l'info'</span></a></li>
 					</ul>
 				</div>
 </footer>
