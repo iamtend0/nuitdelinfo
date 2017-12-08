90563c37d371cdfdd22f224b8bdc8520cbcf5642
saracalafuri <saracalafuri@gmail.com>
2017-12-08 05:54:15 +0100

    page urgence créée

---
+++ urgence.php
@@ -0,0 +1,50 @@
+﻿<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<title>Numeros d'urgence</title>
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<link rel="stylesheet" href="theme/css/style.css" />
+	</head>
+	
+	<body class="subpage">
+	<?php include("header.php");?>
+	
+		<!-- One -->
+		<section id="One" class="wrapper style3">
+			<div class="inner">
+				<header class="align-center">
+					<p>APPG</p>
+					<h2>Numéros d'urgence</h2>
+				</header>
+			</div>
+		</section>
+
+				<!-- Two -->
+			<section id="two" class="wrapper style2">
+				<div class="inner">
+					<div class="box">
+						<div class="content">
+							<header class="align-center">
+								<p>Sécurite routière</p>
+								<h2>Les numéros à contacter en cas d'urgence</h2>
+							</header>
+							<div class="align-center">
+								Les pompiers : 18
+								<br />
+								Le SAMU : 15
+								<br />
+								La police : 17
+								<br />
+								Appel d'urgence européen : 112
+								<br />
+								Numéro d'urgence disponible par SMS pour les personnes sourdes et malentendantes : 114
+							</div>
+							</form>
+						</div>
+					</div>
+				</div>
+			</section>
+		<?php include("footer.php");?>
+	</body>
+</html>
