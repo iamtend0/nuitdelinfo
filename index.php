fe9b38cc8dc23da51466adc93798a67c927529ce
Clément Rogé <you@example.com>
2017-12-08 03:01:20 +0100

    index

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+	<head>
+		<title>Projet Sécurité routière</title>
+	</head>
+	<body>
+		<video controls src="C'est pas sorcier:La sécurité routière.mp4"> C'est pas sorcier: la sécurité routière
+		</video>
+		<br>
+		<p>Le jeu pour choisir sam <a href="sam.php">ici </a></p>
+		<p>Est-ce que tu peux conduire: fais le test  <a href="formulaire.php">ici</a>!</p>
+	</body>
+</html>
