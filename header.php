b440a7e00977ff710737e6cad4ae279b561e8420
nicolai <nicolai@dagestad.fr>
2017-12-08 04:36:31 +0100

    Jean Neymare

--- header.php
+++ header.php
@@ -1,31 +1,30 @@
 <header>
-	<a href="#menu">
-	Menu
-	</a>
-	<script src="js/radio.js" type="text/javascript">
-	</script>
-	<div id="radio">
-		<audio controls>
-			<source id="radioSource"
-				type="audio/mpeg" 
-				src="http://listen.radioking.com/radio/116593/stream/155905"  
-				data-info-album-art=""
-				data-info-album-title=""
-				data-info-artist=""
-				data-info-title=""
-			>
-			Your browser does not support this audio format
-		</audio>
-		<input type="button" onclick="getInfo()" />
-		<span id="timestamp" ></span>
-	</div>
-	<nav id="menu">
-		<ul class="links">
-			<li><a>Home</a></li>
-			<li><a>Sam</a></li>
-			<li><a>Une page introuvable?</a></li>
-			<li><a>Jeu limites de vitesses</a></li>
-			<li><a>Est-ce que tu peux conduire</a></li>
-		</ul>
-	</nav>
+	<div class="logo"><a href="index.html">APPG</a></   div>
+	<a href="#menu">Menu</a>
 </header>
+<script src="js/radio.js" type="text/javascript">
+</script>
+<div id="radio">
+	<audio controls>
+		<source id="radioSource"
+			type="audio/mpeg" 
+			src="http://listen.radioking.com/radio/116593/stream/155905"  
+			data-info-album-art=""
+			data-info-album-title=""
+			data-info-artist=""
+			data-info-title=""
+		>
+		Your browser does not support this audio format
+	</audio>
+	<input type="button" onclick="getInfo()" />
+	<span id="timestamp" ></span>
+</div>
+<nav id="menu">
+	<ul class="links">
+		<li><a>Home</a></li>
+		<li><a>Sam</a></li>
+		<li><a>Une page introuvable?</a></li>
+		<li><a>Jeu limites de vitesses</a></li>
+		<li><a>Est-ce que tu peux conduire</a></li>
+	</ul>
+</nav>
