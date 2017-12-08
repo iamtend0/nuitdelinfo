7814d593942bb2bdd39599f9dacbb396f1c4aeaf
nicolai <nicolai@dagestad.fr>
2017-12-08 03:47:18 +0100

    Nanananan

--- header.php
+++ header.php
@@ -1,4 +1,6 @@
 <header>
+	<script src="js/radio.js" type="text/javascript">
+	</script>
 	<div id="radio">
 		<audio controls>
 			<source type="audio/mpeg" src="http://listen.radioking.com/radio/116593/stream/155905" 
@@ -11,16 +13,16 @@
 			>
 			Your browser does not support this audio format
 		</audio>
-		<input type="button" onlcick="play-pause()" />
+		<input type="button" onclick="getInfo()" />
 		<span id="timestamp" ></span>
 	</div>
-	<nav>
-		<ul>
+	<nav id="menu">
+		<ul class="links">
 			<li><a>Home</a></li>
 			<li><a>Sam</a></li>
 			<li><a>Une page introuvable?</a></li>
 			<li><a>Jeu limites de vitesses</a></li>
-			<li><a>Est-ce que tu peut conduire</a></li>
+			<li><a>Est-ce que tu peux conduire</a></li>
 		</ul>
 	</nav>
 </header>
